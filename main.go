0db31ae4f0774c7389dca5ab721ce69054340638
Iszak Bryan <38895+iszak@users.noreply.github.com>
2018-10-14 20:26:27 +0100

    Implemented points and comments

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"errors"
 	"flag"
-	"fmt"
 	"golang.org/x/net/html"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 // We must export it to allow JSON to marshal it
@@ -23,8 +25,8 @@ type Post struct {
 
 type Posts []Post
 
-
 type comparator func(node *html.Node) bool
+
 func findNode(n *html.Node, compare comparator) []*html.Node {
 	matches := make([]*html.Node, 0)
 	if n == nil {
@@ -39,6 +41,16 @@ func findNode(n *html.Node, compare comparator) []*html.Node {
 	matches = append(matches, findNode(n.NextSibling, compare)...)
 
 	return matches
+}
+
+func prevSiblingUntil(node *html.Node, compare comparator) *html.Node {
+	for node.PrevSibling != nil {
+		node = node.PrevSibling
+		if node != nil && compare(node) {
+			return node
+		}
+	}
+	return nil
 }
 
 func hasAttribute(key string, value string, attrs []html.Attribute) bool {
@@ -71,20 +83,14 @@ func getAttribute(key string, attrs []html.Attribute) *html.Attribute {
 	return nil
 }
 
-func findPosts(n *html.Node) bool {
-	if n.Type != html.ElementNode {
-		return false
+func findByClass(class string) comparator {
+	return func(n *html.Node) bool {
+		if n.Type != html.ElementNode {
+			return false
+		}
+
+		return hasAttribute("class", class, n.Attr)
 	}
-
-	return hasAttribute("class", "athing", n.Attr)
-}
-
-func findTitle(n *html.Node) bool {
-	if n.Type != html.ElementNode {
-		return false
-	}
-
-	return hasAttribute("class", "storylink", n.Attr)
 }
 
 func getUri(nodes []*html.Node) (string, error) {
@@ -111,7 +117,25 @@ func getUri(nodes []*html.Node) (string, error) {
 
 func getTitle(nodes []*html.Node) (string, error) {
 	if len(nodes) != 1 {
-		return "", errors.New("title nodes length is not exactly one")
+		return "", errors.New("author nodes length is not exactly one")
+	}
+
+	firstChild := nodes[0].FirstChild
+	if firstChild == nil {
+		return "", errors.New("author node does not have any children")
+	}
+
+	// TODO: Ideally we should have a "innerText" sort of method here.
+	if firstChild.Type != html.TextNode {
+		return "", errors.New("author node child is not a text node")
+	}
+
+	return firstChild.Data, nil
+}
+
+func getAuthor(nodes []*html.Node) (string, error) {
+	if len(nodes) != 1 {
+		return "", errors.New("author nodes length is not exactly one")
 	}
 
 	firstChild := nodes[0].FirstChild
@@ -127,7 +151,57 @@ func getTitle(nodes []*html.Node) (string, error) {
 	return firstChild.Data, nil
 }
 
-func main()  {
+func getPoints(nodes []*html.Node) (int, error) {
+	if len(nodes) != 1 {
+		return -1, errors.New("point nodes length is not exactly one")
+	}
+
+	firstChild := nodes[0].FirstChild
+	if firstChild == nil {
+		return -1, errors.New("point node does not have any children")
+	}
+
+	if firstChild.Type != html.TextNode {
+		return -1, errors.New("point node child is not a text node")
+	}
+
+	points, err := strconv.Atoi(strings.Replace(firstChild.Data, " points", "", 1))
+	if err != nil {
+		return -1, errors.New("point failed to convert to integer")
+	}
+
+	return points, nil
+}
+
+func getComments(node *html.Node) (int, error) {
+	if node == nil {
+		return -1, errors.New("comment node is nil")
+	}
+
+	textNode := node.FirstChild
+	if node == nil {
+		return -1, errors.New("comment node does not have any children")
+	}
+
+	if textNode.Type != html.TextNode {
+		return -1, errors.New("comment node child is not a text node")
+	}
+
+	if textNode.Data == "discuss" {
+		return 0, nil
+	}
+
+	var re = regexp.MustCompile(`\D*comments?`)
+
+	comments, err := strconv.Atoi(re.ReplaceAllString(textNode.Data, ""))
+	if err != nil {
+		return -1, errors.New("comments failed to convert to integer")
+	}
+
+	return comments, nil
+}
+
+func main() {
 	var postsToFetch int
 
 	flags := flag.NewFlagSet("main", flag.ExitOnError)
@@ -170,19 +244,54 @@ func main()  {
 func getPosts(node *html.Node) (Posts, error) {
 	// NOTE: we could make this allocation more efficient by passing in the length and allocating up front
 	posts := make(Posts, 0)
-	for _, postNode := range findNode(node, findPosts) {
-		// NOTE: we could optimize below to avoid doing 2 tree searches
-		title, err := getTitle(findNode(postNode.FirstChild, findTitle))
-		uri, err := getUri(findNode(postNode.FirstChild, findTitle))
-		// TODO: Implement author, points, comments count and rank
+	for _, postNode := range findNode(node, findByClass("athing")) {
+		titleNodes := findNode(postNode.FirstChild, findByClass("storylink"))
 
+		title, err := getTitle(titleNodes)
 		if err != nil {
 			return nil, err
 		}
 
+		uri, err := getUri(titleNodes)
+		if err != nil {
+			return nil, err
+		}
+
+		nextRow := postNode.NextSibling.FirstChild
+		if nextRow == nil {
+			continue
+		}
+
+		// TODO: Handle ads, with no author
+		author, err := getAuthor(findNode(nextRow, findByClass("hnuser")))
+		if err != nil {
+			//return nil, err
+		}
+
+		points, err := getPoints(findNode(nextRow, findByClass("score")))
+		if err != nil {
+			//return nil, err
+		}
+
+		subText := findNode(nextRow, findByClass("subtext"))
+		// TODO: Check if subtext is nil
+
+		// We may be able to just do subText.LastChild
+		commentNode := prevSiblingUntil(subText[0].LastChild, func(node *html.Node) bool {
+			return node.Type == html.ElementNode
+		})
+		comments, err := getComments(commentNode)
+		if err != nil {
+			//return nil, err
+		}
+		// TODO: Implement rank
+
 		post := Post{
-			Title: title,
-			URI: uri,
+			Title:  title,
+			URI:    uri,
+			Author: author,
+			Points: points,
+			Comments: comments,
 		}
 		posts = append(posts, post)
 	}
